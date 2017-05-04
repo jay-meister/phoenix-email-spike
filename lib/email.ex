7340ecdd2603ff1b21fd2bf5480a635e7fb2ddff
Jack Murphy <jmurphy.web@gmail.com>
2017-05-04 13:25:52 +0100

    add html templates

--- lib/email.ex
+++ lib/email.ex
@@ -1,11 +1,24 @@
 defmodule EmailSpike.Email do
   use Bamboo.Phoenix, view: EmailSpike.EmailView
 
+  def welcome_html_email(email_address) do
+    welcome_text_email(email_address)
+    |> put_html_layout({EmailSpike.LayoutView, "email.html"})
+    |> render("welcome.html")
+  end
+
+
   def welcome_text_email(email_address) do
-    new_email
+    base_email()
     |> to(email_address)
-    |> from("jmurphy.web@gmail.com")
     |> subject("Welcome!")
-    |> text_body("Hello")
+    |> put_text_layout({EmailSpike.LayoutView, "email.text"})
+    |> render("welcome.text")
+  end
+
+  defp base_email do
+    new_email()
+    |> from("jmurphy.web@gmail.com")
+    |> put_header("Reply-To", "jmurphy.web@gmail.com")
   end
 end
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,7 +2,7 @@ defmodule EmailSpike.PageController do
   use EmailSpike.Web, :controller
 
   def index(conn, _params) do
-    EmailSpike.Email.welcome_text_email("jmurphy.web@gmail.com") |> EmailSpike.Mailer.deliver_now
+    EmailSpike.Email.welcome_html_email("jmurphy.web@gmail.com") |> EmailSpike.Mailer.deliver_now
     render conn, "index.html"
   end
 end
